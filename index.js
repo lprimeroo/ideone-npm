c88ecc4c72007a90c16a3597933a4779d7fa3229
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-06-03 10:16:40 +0530

    Initial Push

---
+++ index.js
@@ -0,0 +1,23 @@
+
+module.exports = function (username,passw) {
+
+	var languages = {
+
+	} ;
+
+	var userId = username ,
+		password = passw ;
+
+	var methods = {
+		Run: function (lang,code) {
+			
+			return answer ;
+		} ,
+		Cases: function () {
+
+		} ,
+
+	} ;
+
+	return methods ;
+}
